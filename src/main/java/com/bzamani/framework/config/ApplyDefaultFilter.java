b7f1af8b469831da40487f8265fe658b81eb3343
bashir <bashir.z2004@gmail.com>
2020-12-16 12:22:42 +0330

    add aspect to support apply hibernate filter

---
+++ src/main/java/com/bzamani/framework/config/ApplyDefaultFilter.java
@@ -0,0 +1,30 @@
+package com.bzamani.framework.config;
+
+import com.bzamani.framework.config.security.SecurityUtility;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.hibernate.Session;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+
+@Aspect
+@Component
+class ApplyDefaultFilter {
+
+    @AfterReturning(
+            pointcut = "bean(entityManagerFactory) && execution(* createEntityManager(..))",
+            returning = "retVal")
+    public void getSessionAfter(JoinPoint joinPoint, Object retVal) {
+        if (retVal != null && EntityManager.class.isInstance(retVal) && SecurityContextHolder.getContext().getAuthentication() != null && SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof UserDetails) {
+            Session session = ((EntityManager) retVal).unwrap(Session.class);
+            session.enableFilter("organizationAuthorize").setParameter("username", SecurityContextHolder.getContext().getAuthentication() != null ? ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername() : "");
+        }
+    }
+
+}
+
+
