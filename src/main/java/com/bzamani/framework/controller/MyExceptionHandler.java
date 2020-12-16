cf0982b9ba4ed59df4f1b4242d649039d782631d
bashir <bashir.z2004@gmail.com>
2020-12-16 22:45:58 +0330

    add exception handler

---
+++ src/main/java/com/bzamani/framework/controller/MyExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.bzamani.framework.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class MyExceptionHandler extends ResponseEntityExceptionHandler {
+    // Catch file size exceeded exception!
+    @ExceptionHandler(MultipartException.class)
+    @ResponseBody
+    ResponseEntity<?> handleControllerException(HttpServletRequest request, Throwable ex) {
+        HttpStatus status = getStatus(request);
+        System.out.println("Error: " + ex.getMessage());
+        return new ResponseEntity(ex.getMessage(), status);
+    }
+
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode == null) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return HttpStatus.valueOf(statusCode);
+    }
+
+}
