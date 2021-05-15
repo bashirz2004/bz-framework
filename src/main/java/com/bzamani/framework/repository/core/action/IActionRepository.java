db94a16227ff64d3fbcfece69c5fc80dd4252c36
bashir_z2004 <b.zamani@fanap.ir>
2021-05-15 11:22:00 +0430

    تغییر داشبورد و منو

--- src/main/java/com/bzamani/framework/repository/core/action/IActionRepository.java
+++ src/main/java/com/bzamani/framework/repository/core/action/IActionRepository.java
@@ -17,20 +17,20 @@ public interface IActionRepository extends JpaRepository<Action, Long> {
     @Query("select distinct a FROM User u join u.groups g join g.actions a where u.id = :userId and a.menu = true order by a.sortOrder ")
     List<Action> loadMenuForCurrentUser(@Param("userId") long userId);
 
-    @Query("	select  distinct new com.bzamani.framework.dto.MenuTreeNodeDto(              " +
-            "		p.id as id,                                                                    " +
-            "		p.title as title,															   " +
-            "		p.parent.id as parentId,                                                       " +
-            "       p.sortOrder as sortOrder , " +
-            "       p.src as src," +
-            "       p.iconClass as iconClass ) 													   " +
-            "	from Group g join g.actions p                                                                     " +
+    @Query("	select  distinct new com.bzamani.framework.dto.MenuTreeNodeDto(                     " +
+            "		p.id as id,                                                                     " +
+            "		p.title as title,															    " +
+            "		p.parent.id as parentId,                                                        " +
+            "       p.sortOrder as sortOrder ,                                                      " +
+            "       p.src as src,                                                                   " +
+            "       p.iconClass as iconClass ) 												 	    " +
+            "	from Group g join g.actions p                                                       " +
             "	where p.hierarchyCode in (                                                          " +
             "              select substring(act.hierarchyCode,1,length(p.hierarchyCode))            " +
             "   	          from User u join u.groups ug join ug.actions ua,Action act            " +
             "   	        where u.id = :userId and ua.id = act.id   )                             " +
-            "     and p.parent.id = :parentId " +
-            "     and p.menu = true                                                 " +
+            "     and p.parent.id = :parentId                                                       " +
+            "     and p.menu = true                                                                 " +
             "   order by p.sortOrder                                                                ")
     List<MenuTreeNodeDto> getAuthorizeActionsForUserId(@Param("userId") long userId, @Param("parentId") Long parentId);
 
